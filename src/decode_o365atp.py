bb0d4326ed6efd950b9c45bfc5e4d4f2abadd78d
Jody Foo <jody.foo@gmail.com>
2021-04-22 19:09:40 +0200

    Fixat bugg vid split av SafeLink.

--- src/decode_o365atp.py
+++ src/decode_o365atp.py
@@ -2,14 +2,13 @@
 """Decode and print URL of Office 365 SafeLink."""
 
 import os
-import re
 from urllib.parse import unquote
 
 safelink = os.environ['POPCLIP_TEXT']
 
 if safelink and safelink.find('safelinks.protection.outlook.com/?url='):
     try:
-        params = safelink.split('?')[1].split('&amp;')
+        params = safelink.split('?')[1].split('&')
         params = {
             k: unquote(v) for k, v in (param.split('=') for param in params)
         }
